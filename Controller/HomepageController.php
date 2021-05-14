56ce65dc76ee3bb28c6e41139312f28c623fb6cb
KaiHelsen <kai@Helsen.info>
2021-05-14 13:47:09 +0200

    broke the scene system. enjoy :^)

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -8,38 +8,61 @@ use Model\Transition;
 
 class HomepageController
 {
-    public function render(){
+    public function render()
+    {
 
         session_start();
 
         $scenes = [
+            'openingscene' => new Scene('Welcome to the game,', 'The world is gone. The only thing left in the rubble is a road. The way up is blocked by angry crows! To the right you see a skeleton and zombies! To the left there are traces of a unicorn. Which way do you go?'),
             'unicorn' => new Scene('Search for the unicorn.', 'You are looking for the unicorn. But you do not see him, only some hoofs left in the poop. But there is something else in the poop... namely, a key! All shiny.'),
             'zombiefight' => new Scene('Fight against the zombies!', 'Do you fight the zombies or do you walk away? After all, there a super slow!'),
-            'openingscene' => new Scene('Welcome to the game,', 'The world is gone. The only thing left in the rubble is a road. The way up is blocked by angry crows! To the right you see a skeleton and zombies! To the left there are traces of a unicorn. Which way do you go?')
+            'pitOfDoom' => new Scene("big pit of doom!", "before you, the path suddenly halts as a giant crevice opens in the earth and stops your progress. you have no choice but to return to whence you came."),
         ];
 
         $scenes['openingscene']->addTransition(new Transition('left', $scenes['unicorn']));
         $scenes['openingscene']->addTransition(new Transition('right', $scenes['zombiefight']));
 
-        $activeScene = $scenes['openingscene'];
+        $scenes['unicorn']->addTransition(new Transition('left', $scenes['pitOfDoom']));
+        $scenes['unicorn']->addTransition(new Transition('right', $scenes['openingscene']));
 
-        if (isset($_POST['player'])) {
+        $scenes['pitOfDoom']->addTransition(new Transition('left', $scenes['unicorn']));
+
+        if (!isset($_SESSION['currentScene']))
+        {
+            $_SESSION['currentScene'] = $scenes['openingscene'];
+        }
+        $activeScene = $_SESSION['currentScene'];
+
+
+
+        if (isset($_POST['player']))
+        {
             $player = new Player($_POST['player']);
             $_SESSION['player'] = $player;
 
-        } else if (isset($_SESSION['player'])) {
+        }
+        else if (isset($_SESSION['player']))
+        {
             $player = $_SESSION['player'];
-        } else {
+        }
+        else
+        {
             $player = new Player('Dummy');
         }
 
-        if (!empty($_GET['command'])) {
-            $activeScene = $activeScene->findValidTransition($_GET['command']);
+        if (!empty($_GET['command']))
+        {
 
-            if ($activeScene === null) {
+             $nextScene = $_SESSION['currentScene']->findValidTransition($_GET['command']);
+
+            if ($nextScene=== null)
+            {
                 die('Someboy or somegirl do this better than me!');//@todo!
             }
+            $activeScene = $nextScene;
         }
+
 
         require 'View/homepage.php';
     }

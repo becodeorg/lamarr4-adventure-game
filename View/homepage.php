576145a0b2e79762909b906555aec466ce4bb91a
Micha <michaelaharin@gmail.com>
2021-06-24 13:52:20 +0200

    unicorn scene visual created

--- View/homepage.php
+++ View/homepage.php
@@ -74,27 +74,8 @@ include './View/includes/header.php';
 
 
     </section>
-    <div class="background-container">
-        <div class="layer background"></div>
-        <div class="layer moon"></div>
-        <div class="layer moutain"></div>
-        <div class="layer fog1"></div>
-        <div class="layer hills"></div>
-        <div class="layer fog2"></div>
-        <div class="layer crows"></div>
-        <div class="layer unicorn"></div>
-        <div class="layer foreground"></div>
-    </div>
-
-    <div class="container">
-        <div class="dragon wings">
-        </div>
-        <div class="dragon body">
-        </div>
-        <div class="dragon head">
-        </div>
-    </div>
-
+    <?php
+include './View/includes/openingScene.php'; ?>
 </main>
 
 
---
+++ View/includes/unicornScene.php
@@ -0,0 +1,5 @@
+<div class="background-container">
+    <div class="layer unicornFront"></div>
+    <div class="layer unicornBack"></div>
+    <div class="layer unicornPoop"></div>
+</div>
